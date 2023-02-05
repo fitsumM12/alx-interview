75aa189bd4cf39145a61a2d720ebc2a6a4d6d02f
fitsumM12 <fitsummesfindejene@gmail.com>
2023-02-05 22:48:33 +0530

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,72 +1,140 @@
 #!/usr/bin/python3
 """
-Module 0-nqueens
-A program that solves the N queens problem
+   Description: The N queens puzzle is the challenge of placing N non-attacking
+                queens on an N×N chessboard. Write a program that solves the N
+                queens problem.
+   Usage: nqueens N:
+          If the user called the program with the wrong number of arguments,
+          print Usage: nqueens N, followed by a new line, and exit with the
+          status 1
+   where N must be an integer greater or equal to 4:
+          If N is not an integer, print N must be a number, followed by a new
+          line, and exit with the status 1
+          If N is smaller than 4, print N must be at least 4, followed by a new
+          line, and exit with the status 1
+   The program should print every possible solution to the problem:
+          One solution per line
+          Format: see example
+          You don’t have to print the solutions in a specific order
+   You are only allowed to import the sys module
 """
+
+
 import sys
 
-if len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
 
-try:
-    num = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
+def print_board(board):
+    """ print_board
+    Args:
+        board - list of list with length sys.argv[1]
+    """
+    new_list = []
+    for i, row in enumerate(board):
+        value = []
+        for j, col in enumerate(row):
+            if col == 1:
+                value.append(i)
+                value.append(j)
+        new_list.append(value)
 
-if not (num >= 4):
-    print("N must be at least 4")
-    sys.exit(1)
+    print(new_list)
 
 
-def solveNQueens(n):
-    """Solution for n queens"""
-    col = set()  # keep track of used columns
-    pos = set()  # (r + c) keep track of used positive diagonals
-    neg = set()  # (r - c) keep track of used negative diagonals
+def isSafe(board, row, col, number):
+    """ isSafe
+    Args:
+        board - list of list with length sys.argv[1]
+        row - row to check if is safe doing a movement in this position
+        col - col to check if is safe doing a movement in this position
+        number: size of the board
+    Return: True of False
+    """
 
-    res = []  # final result
+    # Check this row in the left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
 
-    board = [[] for n in range(n)]  # create empy board
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
 
-    def backtrack(row):
-        """function for recursion"""
-        # means we've reached the last row
-        if row == n:
-            # get copy of current solution(current board)
-            copy = board.copy()
-            res.append(copy)
-            return
+    for i, j in zip(range(row, number, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
 
-        # for every column
-        for c in range(n):
-            # if we find that the column or diagonals are used, then skip
-            if c in col or (row + c) in pos or (row - c) in neg:
-                continue
+    return True
 
-            # register found columns and diagonals
-            col.add(c)
-            pos.add(row + c)
-            neg.add(row - c)
 
-            board[row] = [row, c]
+def solveNQUtil(board, col, number):
+    """ Auxiliar method to find the posibilities of answer
+    Args:
+        board - Board to resolve
+        col - Number of col
+        number - size of the board
+    Returns:
+        All the posibilites to solve the problem
+    """
 
-            # move to next row
-            backtrack(row + 1)
+    if (col == number):
+        print_board(board)
+        return True
+    res = False
+    for i in range(number):
 
-            # finally undo
-            col.remove(c)
-            pos.remove(row + c)
-            neg.remove(row - c)
-            board[row] = []
+        if (isSafe(board, i, col, number)):
 
-    backtrack(0)
+            # Place this queen in board[i][col]
+            board[i][col] = 1
+
+            # Make result true if any placement
+            # is possible
+            res = solveNQUtil(board, col + 1, number) or res
+
+            board[i][col] = 0  # BACKTRACK
 
     return res
 
 
+def solve(number):
+    """ Find all the posibilities if exists
+    Args:
+        number - size of the board
+    """
+    board = [[0 for i in range(number)]for i in range(number)]
+
+    if not solveNQUtil(board, 0, number):
+        return False
+
+    return True
+
+
+def validate(args):
+    """ Validate the input data to verify if the size to
+        answer is posible
+    Args:
+        args - sys.argv
+    """
+    if (len(args) == 2):
+        # Validate data
+        try:
+            number = int(args[1])
+        except Exception:
+            print("N must be a number")
+            exit(1)
+        if number < 4:
+            print("N must be at least 4")
+            exit(1)
+        return number
+    else:
+        print("Usage: nqueens N")
+        exit(1)
+
+
 if __name__ == "__main__":
-    boards = solveNQueens(num)
-    for board in boards:
-        print(board)
+    """ Main method to execute the application
+    """
+
+    number = validate(sys.argv)
+    solve(number)
--- 0x05-nqueens/README.md
+++ 0x05-nqueens/README.md
@@ -1,30 +1,56 @@
-# N queens
+![](Top.png)
 
-The N queens puzzle is a challenge of placing N non-attacking queens on an NxN chessboard.
+# Requirements
 
-On a chessboard, the queen can move any number of squares vertically, horizontally
-or diagonally.Thus, to solve the puzzle, each queen must be placed on its own
-row, column and diagonal.
+## General
 
-This is most efficiently solved using a backtracking algorithm. Backtracking is
-a general algorithm for finding solutions to some computational problems that
-incrementally builds candidates to the solutions, and abandons a candidate i.e.,
-backtracks as soon as it determines that the candidate cannot possibly be completed
-to a valid solution.
+> - Allowed editors: vi, vim, emacs
+> - All your files will be interpreted/compiled on Ubuntu 14.04 LTS using python3 (version 3.4.3)
+> - All your files should end with a new line
+> - The first line of all your files should be exactly #!/usr/bin/python3
+> - A README.md file, at the root of the folder of the project, is mandatory
+> - Your code should use the PEP 8 style (version 1.7.x)
+> - All your files must be executable
 
-## Usage
 
-```nqueens N```
+## Whiteboarding
 
-where N is the number of queens.
-      - N must be an integer greater or equal to 4
-      - the program prints every possible solution to the problem in the format
-        shown below
+![](whiteboard1.jpg)
 
-```
-$ ./0-nqueens 4
+## Task
+
+**0. N queens**
+
+File: [0-nqueens.py](0-nqueens.py/)
+
+Chess grandmaster Judit Polgár, the strongest female chess player of all time
+
+
+The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard. Write a program that solves the N queens problem.
+
+- Usage: nqueens N
+> - If the user called the program with the wrong number of arguments, print Usage: nqueens N, followed by a new line, and exit with the status 1
+
+- where N must be an integer greater or equal to 4
+> - If N is not an integer, print N must be a number, followed by a new line, and exit with the status 1
+> - If N is smaller than 4, print N must be at least 4, followed by a new line, and exit with the status 1
+
+- The program should print every possible solution to the problem
+> - One solution per line
+> - Format: see example
+> - You don’t have to print the solutions in a specific order
+
+- You are only allowed to import the sys module
+
+```sh
+julien@ubuntu:~/0x08. N Queens$ ./0-nqueens.py 4
 [[0, 1], [1, 3], [2, 0], [3, 2]]
 [[0, 2], [1, 0], [2, 3], [3, 1]]
+julien@ubuntu:~/0x08. N Queens$ ./0-nqueens.py 6
+[[0, 1], [1, 3], [2, 5], [3, 0], [4, 2], [5, 4]]
+[[0, 2], [1, 5], [2, 1], [3, 4], [4, 0], [5, 3]]
+[[0, 3], [1, 0], [2, 4], [3, 1], [4, 5], [5, 2]]
+[[0, 4], [1, 2], [2, 0], [3, 5], [4, 3], [5, 1]]
+julien@ubuntu:~/0x08. N Queens$ 
 ```
 
-Each inner list represents the coordinates of a queen in the NxN chessboard.
